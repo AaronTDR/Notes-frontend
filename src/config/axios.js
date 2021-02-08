88711a29af82f8551d5076410996876832db62ca
Aaron <aaron.tdr12@gmail.com>
2021-02-07 21:01:08 -0600

    Add environment variables in frontend/.env.development

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const axiosCustomer = axios.create({});
+const axiosCustomer = axios.create({
+  baseURL: process.env.REACT_APP_BACKEND_URL,
+});
 
 export default axiosCustomer;
